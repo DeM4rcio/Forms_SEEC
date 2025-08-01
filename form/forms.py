430bb355383058d3a84b00898c200ff87ed25b42
DeM4rcio <henriquemarcio170@gmail.com>
2025-08-01 01:16:31 -0300

    add

--- form/forms.py
+++ form/forms.py
@@ -4,12 +4,14 @@ from .models import RespostaUnica
 
 class FormularioEscolha(forms.ModelForm):
     class Meta:
-       opcao = forms.ChoiceField(
-        choices=RespostaUnica.OPCOES,
-        widget=forms.RadioSelect,
-        empty_label=None  # Adicione esta linha
-    )
-
-    class Meta:
         model = RespostaUnica
-        fields = ['opcao'] 
+        fields = ['opcao']
+        widgets = {
+            'opcao': forms.RadioSelect
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove a primeira opção (vazia) do ChoiceField se ela existir
+        if self.fields['opcao'].choices and self.fields['opcao'].choices[0][0] == '':
+            self.fields['opcao'].choices = self.fields['opcao'].choices[1:]
