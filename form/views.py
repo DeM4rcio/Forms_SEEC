e737f667d7d6ca4f5827ec13cc08cc999eaf5eee
DeM4rcio <henriquemarcio170@gmail.com>
2025-08-01 00:16:08 -0300

    add

--- form/views.py
+++ form/views.py
@@ -1,4 +1,6 @@
 import json
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -40,19 +42,22 @@ def autenticar_usuario(request):
 
         # 1. Determinar o domínio e as configurações do servidor
         if "@economia.df.gov.br" in email:
+            dominio = "SEAP"
             ldap_server = Server(settings.LDAP_SERVER, get_info=ALL)
             base_dn = 'dc=seap,dc=gdfnet,dc=df'
-            servico_user = settings.LDAP_USERNAME
+            servico_user = settings.LDAP_USERNAME # UPN do usuário de serviço
             servico_pass = settings.LDAP_PASSWORD
         elif "@gdfnet.df.gov.br" in email:
+            dominio = "GDFNET"
             ldap_server = Server(settings.LDAP_SERVER_NET, get_info=ALL)
             base_dn = 'dc=gdfnet,dc=df'
-            servico_user = settings.LDAP_USERNAME_NET
+            servico_user = settings.LDAP_USERNAME_NET # UPN do usuário de serviço
             servico_pass = settings.LDAP_PASSWORD_NET
         else:
             return JsonResponse({'success': False, 'message': 'Domínio de e-mail não reconhecido.'}, status=400)
 
         # 2. Conexão com o usuário de serviço para buscar o DN do usuário
+        # Removendo 'authentication=NTLM' e ajustando 'user' para o UPN
         try:
             with Connection(
                 ldap_server,
@@ -72,21 +77,6 @@ def autenticar_usuario(request):
 
                 user_entry = conn.entries[0]
                 user_dn = user_entry.entry_dn
-
-        except LDAPBindError as e:
-            return JsonResponse({'success': False, 'message': 'Erro ao conectar com o servidor LDAP. Verifique as credenciais de serviço.'}, status=500)
-
-        # 3. Tentativa de autenticação com a senha do usuário
-        try:
-            with Connection(
-                ldap_server,
-                user=user_dn,
-                password=senha,
-                auto_bind=True
-            ):
-                # Se chegou aqui, a autenticação LDAP foi bem-sucedida
-                
-                # 4. Login no Django para criar a sessão
                 try:
                     # Tente encontrar o usuário no banco de dados do Django
                     user = User.objects.get(username=email)
@@ -100,6 +90,25 @@ def autenticar_usuario(request):
                 # 5. Redirecione o usuário para a página original ou para a página de sucesso
                 next_url = request.GET.get('next', '/formulario/')
                 return redirect(next_url)
+        except LDAPBindError as e:
+            # Erro na conexão do usuário de serviço
+            print("Erro na conexão do usuário de serviço:", e)
+            return JsonResponse({'success': False, 'message': 'Erro ao conectar com o servidor LDAP. Verifique as credenciais de serviço.'}, status=500)
+
+        # 3. Tentativa de autenticação com a senha do usuário
+        try:
+            with Connection(
+                ldap_server,
+                user=user_dn,
+                password=senha,
+                auto_bind=True
+            ) as user_conn:
+                # Se chegou aqui, a autenticação foi bem-sucedida
+                user_info = {
+                    "Email": user_entry.mail.value if "mail" in user_entry else None,
+                    "Nome": user_entry.cn.value if "cn" in user_entry else None
+                }
+                return JsonResponse(user_info, status=200)
 
         except LDAPBindError:
             # Erro de autenticação, senha incorreta
@@ -113,6 +122,8 @@ def autenticar_usuario(request):
     except Exception as e:
         print("Erro na autenticação:", e)
         return JsonResponse({'success': False, 'message': f'Erro interno ao autenticar. Tente novamente mais tarde.'}, status=500)
+
+
 @login_required(login_url='login')  
 def formulario_view(request):
     try:
