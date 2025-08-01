e5aa93ec8d42922e2535b00afc6d4240a196771a
DeM4rcio <henriquemarcio170@gmail.com>
2025-08-01 01:05:54 -0300

    add

--- form/forms.py
+++ form/forms.py
@@ -4,8 +4,12 @@ from .models import RespostaUnica
 
 class FormularioEscolha(forms.ModelForm):
     class Meta:
+       opcao = forms.ChoiceField(
+        choices=RespostaUnica.OPCOES,
+        widget=forms.RadioSelect,
+        empty_label=None  # Adicione esta linha
+    )
+
+    class Meta:
         model = RespostaUnica
-        fields = ['opcao']
-        widgets = {
-            'opcao': forms.RadioSelect
-        }
+        fields = ['opcao'] 
